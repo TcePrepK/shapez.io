c77e154f27452342f26da8b8ad0a458621e3335f
TcePrepK <56453014+TcePrepK@users.noreply.github.com>
2021-04-30 13:25:34 +0300

    query

--- src/js/core/query_parameters.js
+++ src/js/core/query_parameters.js
@@ -12,11 +12,7 @@ if (options.embed) {
 }
 
 // Allow testing full version outside of standalone
-if (options.fullVersion && !G_IS_RELEASE) {
+if (!G_IS_RELEASE) {
     queryParamOptions.fullVersion = true;
-}
-
-// Allow testing full version outside of standalone
-if (options.sandboxMode && !G_IS_RELEASE) {
     queryParamOptions.sandboxMode = true;
 }
