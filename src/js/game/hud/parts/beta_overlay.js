95ffd2edb78d208f321655260e5bc7c1cb4dbf06
TcePrepK <56453014+TcePrepK@users.noreply.github.com>
2021-05-07 23:44:41 +0300

    commitHash

--- src/js/game/hud/parts/beta_overlay.js
+++ src/js/game/hud/parts/beta_overlay.js
@@ -2,14 +2,13 @@ import { BaseHUDPart } from "../base_hud_part";
 import { makeDiv } from "../../../core/utils";
 
 export class HUDBetaOverlay extends BaseHUDPart {
-    createElements(parent) {
-        this.element = makeDiv(
-            parent,
-            "ingame_HUD_BetaOverlay",
-            [],
-            "<h2>UNSTABLE BETA VERSION</h2><span>Unfinalized & potential buggy content!</span>"
-        );
-    }
-
-    initialize() {}
+    // createElements(parent) {
+    //     this.element = makeDiv(
+    //         parent,
+    //         "ingame_HUD_BetaOverlay",
+    //         [],
+    //         "<h2>UNSTABLE BETA VERSION</h2><span>Unfinalized & potential buggy content!</span>"
+    //     );
+    // }
+    // initialize() {}
 }
